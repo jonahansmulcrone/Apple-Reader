bcbde7696153eb0f5d2b4da4d35af5e8dd42be7d
jonahansmulcrone <126010204+jonahansmulcrone@users.noreply.github.com>
2025-10-23 21:44:41 -0700

    Created vite configs for building chrome extension dist for vue3 application

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -9,10 +10,43 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    viteStaticCopy({
+     targets: [
+       {
+         src: 'src/background/background.js', 
+         dest: 'background',
+       },
+       {
+         src: 'public/manifest.json',
+         dest: ''
+       }
+     ],
+   })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popup: 'index.html',                      
+        content: 'src/main.ts',               
+        background: 'src/background/background.js',
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'content') {
+            return 'main.js';                   
+          }
+          if (chunkInfo.name === 'background') {
+            return 'background/background.js';
+          }
+          return '[name].js';
+        },
+      },
+      external: []
     },
   },
 })
